975ff9c5db0dbcbb88def64191c953db77f9a2a6
anatoly62 <mav120962@gmail.com>
2017-02-15 12:41:03 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -345,7 +345,7 @@ pub fn send(s: &str) {
         var xhr = new XMLHttpRequest();\
         xhr.open('POST', 'http://127.0.0.1:8000/');\
         xhr.onload = function() {alert(UTF8ToString(this.responseText));}\
-	    xhr.onerror = function() {alert(UTF8ToString("Error "+this.status));}\
+	    xhr.onerror = function() {alert(UTF8ToString(this.status));}\
 	    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded') ;\
 	    xhr.send(UTF8ToString($0));\
     \0" };
